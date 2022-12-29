66a5e7f847165301c60fa94d77774e423d34e9f5
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-12-29 21:50:36 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import debounce from 'lodash.debounce';
 import './css/styles.css';
-import fetchCountries from '../src/js/fetchCountries'
-
+import fetchCountries from '../js/fetchCountries'
 
 const DEBOUNCE_DELAY = 300;
 
-const searchBox = document.querySelector('#search-box')
+const searchBox = document.querySelector('#search-box');
+
 
 searchBox.addEventListener('input', debounce(() => {
-        fetchCountries(searchBox.value.trim())
-    
+    fetchCountries(searchBox.value.trim())
 }, DEBOUNCE_DELAY))
