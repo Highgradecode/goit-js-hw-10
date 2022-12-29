8a5d2eb3c508dfde916ade612b6dc5d97fa8a283
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-12-29 21:38:27 +0200

    Fixing

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,60 +1,62 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const countryList = document.querySelector('.country-list')
+const searchBox = document.querySelector('#search-box')
 
-
-export default fetchCountries = (name) => {
-    const URL = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
-
+export default function fetchCountries(name) {
+    const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages
+`;
     countryList.textContent = '';
-    if (name !== '') {
+    
+    if (searchBox.value !== "") {
         return fetch(URL)
         .then(response => {
             if (response.ok) {
                 return response.json()
             }
-            return Notify.failure('Oops, there is no country with that name')
+            Notify.failure('"Oops, there is no country with that name"');
         })
-        .then(countries => makeCountriesMarkup(countries))
-        .catch(error => console.error(error))
-    };
+        .then(countries => {
+            renderResult(countries)
+        })
+        .catch(error => error)
+    }
+ 
 }
 
 
-const makeCountriesMarkup = (countriesArr) => {
+const renderResult = (countriesArr) => {
 
-    
     if (countriesArr.length <= 10) {
-        const countriesMarkup = countriesArr.map(country => {
+        const markup = countriesArr.map(country => {
+            if (countriesArr.length !== 1) {
+                return `
+                <li> <img src="${country.flags.svg}" height=30px width=50px>
+                <h2>${country.name.official}</h2></li>
+                `
+                
+            }
+            
+            return `<li> <img src="${country.flags.svg}" height=50px width=100px>
+                <h2>${country.name.official}</h2>
+                <h3>Capital:</h3>
+                <p>${country.capital}</p>
+                <h3>Population:</h3>
+                <p>${country.population}</p>
+                <h3>Languages:</h3>
+                <p>${Object.values(country.languages).join(', ')}</p>
+                </li>
+                `
+        })
+            .join('')
 
-        if (countriesArr.length !== 1) {
-            return `
-        <li class='country-item'>
-            <img src="${country.flags.svg}" alt="flag" height=50px width=83px>
-            <h2>${country.name}</h2>
-        </li>`
-        } else {
-            return `
-            <img src="${country.flags.svg}" alt="flag" height=50px width=83px>
-            <h2>${country.name}</h2>
-            <h3>Capital:</h3>
-            <p>${country.capital}</p>
-            <h3>Population:</h3>
-            <p>${country.population}</p>
-            <h3>Languages:</h3>
-            <p>${country.languages.map(language => {
-                return language.name
-            }).join(', ')}</p>
-            `
-        }
-    }).join('')
-
-    countryList.insertAdjacentHTML('afterbegin', countriesMarkup)
-    } else {
-        return Notify.info('Too many matches found. Please enter a more specific name.')
+            countryList.insertAdjacentHTML('afterbegin', markup)
+    } else{
+        Notify.info("Too many matches found. Please enter a more specific name.");
     }
-    
+
     
 
+   
     
 };
